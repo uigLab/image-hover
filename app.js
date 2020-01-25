f67a3176f143297f4bfbcb33dcf181950ec3d621
UI Galaxy <uigalaxy7@gmail.com>
2020-01-25 22:27:53 +0600

    init app

---
+++ app.js
@@ -0,0 +1,7 @@
+var myAnimation = new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "baby1.jpg",
+  image2: "baby2.jpg",
+  displacementImage: "disp.png"
+});
